05a40fac1f0bc0227c3275b55ab1dc6e731d8465
Empie <54358337+Empie@users.noreply.github.com>
2021-04-16 02:55:35 +0200

    quick fix

--- AnnotationsWidget.py
+++ AnnotationsWidget.py
@@ -7,11 +7,26 @@ class AnnotationsWidget(pg.PlotWidget):
         self.hideAxis('left')
         self.setBackground('w')
         self.setMouseEnabled(x=False, y=False)
-        self.important_annotations = ['Power On', 'Generic', 'Oxygen', 'Nitroglycerin', 'Morphine', 'Epinephrine', 'Atropine', 'Lidocaine','Power Off']
+        self.important_annotations = {'Power On': 'S', 
+                                    'Generic':' G',
+                                    'Oxygen': 'O',
+                                    'Nitroglycerin': 'N',
+                                    'Morphine': 'M',
+                                    'Epinephrine': 'Ep',
+                                    'Atropine': 'A',
+                                    'Lidocaine': 'L',
+                                    'Power Off': 'E'
+                                    }
 
     def _setTags(self, case):
         for i in range(len(case["metadata"]["ann"])):
             if case["metadata"]["ann"][i] in self.important_annotations:
-                self.addItem(pg.InfiniteLine(pos=i, label=case["metadata"]["ann"][i], pen=pg.mkPen('b', width=2), labelOpts={'color': 'w', 'position': 0.7, 'fill': 'b'}))
+                tag = case["metadata"]["ann"][i]
+                if i == 0:
+                    self.addItem(pg.InfiniteLine(pos=i, label=self.important_annotations[tag], pen=pg.mkPen('g', width=2), labelOpts={'color': 'w', 'position': 0.6, 'fill': 'g'}))
+                elif i == len(case["metadata"]["ann"])-1:
+                    self.addItem(pg.InfiniteLine(pos=i, label=self.important_annotations[tag], pen=pg.mkPen('r', width=2), labelOpts={'color': 'w', 'position': 0.6, 'fill': 'r'}))
+                else:
+                    self.addItem(pg.InfiniteLine(pos=i, label=self.important_annotations[tag], pen=pg.mkPen('b', width=2), labelOpts={'color': 'w', 'position': 0.6, 'fill': 'b'}))
 
         
